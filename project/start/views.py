3a8c359a90b6c57df3724d6693d7eff99193c8e7
Артём Смородинов <leckoy700@gmail.com>
2026-03-09 17:12:51 +0300

    Добавил

--- project/start/views.py
+++ project/start/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def start(request):
-    show_picture = False
     if request.method == "POST":
-        show_picture = True
+
+        current = request.GET.get('show', '0')
+        new_value = '0' if current == '1' else '1'
+        return redirect(f'/start/?show={new_value}')
+    
+  
+    show_picture = request.GET.get('show', '0') == '1'
     
     return render(request, "start/start.html", {
         'show_picture': show_picture
